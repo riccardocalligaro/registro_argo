a18f29a273db7929327de4d25ca7ce5d3e8b0ae0
Riccardo Calligaro <riccardocalligaro@gmail.com>
2020-01-11 14:43:20 +0100

    Fixed input bug and overflow, added navigation when success

--- lib/presentation/feature/login/login_page.dart
+++ lib/presentation/feature/login/login_page.dart
@@ -2,8 +2,6 @@ import 'package:f_logs/model/flog/flog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:registro_argo/data/db/dao/profile_dao.dart';
-import 'package:registro_argo/injector_container.dart';
 import 'package:registro_argo/presentation/bloc/auth/bloc.dart';
 import 'package:registro_argo/presentation/feature/home/home_page.dart';
 import 'package:registro_argo/presentation/feature/widgets/buttons/gradient_green_button.dart';
@@ -29,48 +27,63 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, state) {
-        if (state is Init || state is SignInLoadError) {
-          return _buildInitial();
-        } else if (state is SignInLoadInProgress) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (state is SignInLoadSuccess) {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => HomePage()),
-          );
-        }
+    return Scaffold(
+      body: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is SignInLoadSuccess) {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          }
+          if (state is SignInLoadError) {
+            // setState(() {
+            //   _invalid = true;
+            //   _erorrMessage = 'Credenziali invalide';
+            // });
+          }
+        },
+        child: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is Init || state is SignInLoadError) {
+              return _buildInitial();
+            } else if (state is SignInLoadInProgress) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
 
-        return Container();
-      },
-    ));
+            return Text(state.toString());
+          },
+        ),
+      ),
+    );
   }
 
   Widget _buildInitial() {
     return Stack(
       children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(
-                height: 120,
-              ),
-              _buildHeaderText(),
-              _buildLoginInput(),
-            ],
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 90,
+                ),
+                _buildHeaderText(),
+                _buildLoginInput(),
+              ],
+            ),
           ),
         ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: SvgPicture.asset(
-            'assets/images/green_waves.svg',
-          ),
-        ),
+        // Align(
+        //   alignment: Alignment.bottomCenter,
+        //   child: SvgPicture.asset(
+        //     'assets/images/green_waves.svg',
+        //   ),
+        // ),
       ],
     );
   }
@@ -98,118 +111,107 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   Widget _buildLoginInput() {
-    return BlocListener<AuthBloc, AuthState>(
-      listener: (context, state) {
-        // if (state is SignInLoadError) {
-        //   print("erorr");
-        //   setState(() {
-        //     _invalid = true;
-        //     //_erorrMessage = 'Credenziali invalide';
-        //   });
-        // }
-      },
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 30,
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 30,
+        ),
+        TextField(
+          controller: _schoolCodeController,
+          decoration: InputDecoration(
+              hintText: 'School code',
+              errorText: _invalid ? _erorrMessage : null),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextField(
+          controller: _passwordController,
+          decoration: InputDecoration(
+              hintText: 'Username', errorText: _invalid ? _erorrMessage : null),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        TextField(
+          controller: _usernameController,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            errorText: _invalid ? _erorrMessage : null,
           ),
-          TextFormField(
-            controller: _schoolCodeController,
-            decoration: InputDecoration(
-                hintText: 'School code',
-                errorText: _invalid ? _erorrMessage : null),
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        GradientGreenButton(
+          center: const Text(
+            'Accedi',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 19),
           ),
-          SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-            controller: _usernameController,
-            decoration: InputDecoration(
-                hintText: 'Username',
-                errorText: _invalid ? _erorrMessage : null),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-            controller: _passwordController,
-            decoration: InputDecoration(
-              hintText: 'Password',
-              errorText: _invalid ? _erorrMessage : null,
+          onTap: () {
+            FLog.info(text: 'Sign in button pressed');
+
+            FocusScopeNode currentFocus = FocusScope.of(context);
+            if (!currentFocus.hasPrimaryFocus) {
+              currentFocus.unfocus();
+            }
+
+            final schoolCode = _schoolCodeController.text;
+            final username = _usernameController.text;
+            final password = _passwordController.text;
+
+            if (schoolCode != '' && username != '' && password != '') {
+              FLog.info(
+                text: 'Got valid input, proceeding to adding event to bloc',
+              );
+
+              BlocProvider.of<AuthBloc>(context).add(SignIn(
+                schoolCode: schoolCode,
+                username: username,
+                password: password,
+              ));
+            } else {
+              FLog.info(text: 'Got empty input');
+              setState(() {
+                _invalid = true;
+                _erorrMessage = "Devi compilare in tutti i campi";
+              });
+            }
+          },
+        ),
+        // RaisedButton(
+        //   child: Text('requiree db'),
+        //   onPressed: () async {
+        //     final ProfileDao profileDao = ProfileDao(sl());
+        //     final profiles = await profileDao.getLoggedInProfile();
+        //     print(profiles);
+        //   },
+        // ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Sicuro. ',
+              style: TextStyle(color: Colors.grey),
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          GradientGreenButton(
-            center: const Text(
-              'Accedi',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 19),
-            ),
-            onTap: () {
-              FLog.info(text: 'Sign in button pressed');
-
-              // final schoolCode = _schoolCodeController.text;
-              // final username = _usernameController.text;
-              // final password = _passwordController.text;
-
-              final schoolCode = 'SS16585';
-              final username = 'enrico.mognato';
-              final password = 'password';
-
-              if (schoolCode != '' && username != '' && password != '') {
-                FLog.info(
-                  text: 'Got valid input, proceeding to adding event to bloc',
-                );
-
-                BlocProvider.of<AuthBloc>(context).add(SignIn(
-                  schoolCode: schoolCode,
-                  username: username,
-                  password: password,
-                ));
-              } else {
-                FLog.info(text: 'Got empty input');
-                setState(() {
-                  _invalid = true;
-                  _erorrMessage = "Devi compilare in tutti i campi";
-                });
-              }
-            },
-          ),
-          // RaisedButton(
-          //   child: Text('requiree db'),
-          //   onPressed: () async {
-          //     final ProfileDao profileDao = ProfileDao(sl());
-          //     final profiles = await profileDao.getLoggedInProfile();
-          //     print(profiles);
-          //   },
-          // ),
-          SizedBox(
-            height: 20,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Sicuro. ',
-                style: TextStyle(color: Colors.grey),
-              ),
-              GestureDetector(
-                onTap: () {
-                  // todo: navigate to repository
-                },
-                child: Text(
-                  'Open source.',
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                  ),
+            GestureDetector(
+              onTap: () {
+                // todo: navigate to repository
+              },
+              child: Text(
+                'Open source.',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
                 ),
-              )
-            ],
-          )
-        ],
-      ),
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 }
